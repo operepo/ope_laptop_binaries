447dab4a7b54018b26220bbf68dc2bb35863d3dc
Ray Pulsihper <ray@cmagic.biz>
2018-03-06 21:10:31 -0800

    - Remove firewall settings from gpolicy - use netsh command exclusively - Move credential steps into separate batch file so updates take effect immediately - Add netsh advfirewall reset before importing firewall rules

--- bin/import_firewall_rules.cmd
+++ bin/import_firewall_rules.cmd
@@ -1,10 +1,10 @@
 @echo off
 
+rem reset settings to default
+netsh advfirewall reset
+
 rem turn on all profiles
 rem netsh advfirewall set allprofiles state on
-
-rem reset settings
-rem netsh advfirewall reset
 
 rem turn on logging
 rem netsh advfirewall set currentprofile logging filename "c:\programdata\ope\tmp\log\pfirewall.log"
