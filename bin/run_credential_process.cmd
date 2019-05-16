ea823b0cd6d85b6492b2aa97a85fad224494bff6
Ray Pulsipher <ray@cmagic.biz>
2019-05-16 11:02:04 -0700

    .

--- bin/run_credential_process.cmd
+++ bin/run_credential_process.cmd
@@ -79,15 +79,28 @@ call %~dp0trust_ope_certs.cmd
 rem Make sure vstudio redists are installed
 echo -- Installing required packages --
 echo - Installing MSVC Runtimes - 2008...
+call %~dp0vcredist_x64_2008.exe /install /quiet
 call %~dp0vcredist_x86_2008.exe /install /quiet
+
 echo - Installing MSVC Runtimes - 2010...
 rem call %~dp0vcredist_x64_2010.exe /install /quiet
 rem call %~dp0vcredist_x86_2010.exe /install /quiet
+
+echo - Installing MSVC Runtimes - 2012...
+rem call %~dp0vcredist_x64_2012.exe /install /quiet
+rem call %~dp0vcredist_x86_2012.exe /install /quiet
+
+echo - Installing MSVC Runtimes - 2013...
+call %~dp0vcredist_x64_2013.exe /install /quiet
+call %~dp0vcredist_x86_2013.exe /install /quiet
+
 echo - Installing MSVC Runtimes - 2015 to 2019...
 call %~dp0vc_redist.x64_15_to_19.exe /install /quiet
 call %~dp0vc_redist.x86_15_to_19.exe /install /quiet
+
 echo - Installing MSVC Runtimes - LMS redist...
 call %~dp0..\lms\vc_redist.x64.exe /install /quiet
+
 rem call %~dp0Win64OpenSSL_Light-1_1_1b.exe /silent /allusers /nocancel /norestart /closeapplications /restartapplications /noicons
 rem  /verysilent
 
