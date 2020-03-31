be43b2b917dc347746fefb61d039b428e3dc8baf
Ray Pulsihper <ray@cmagic.biz>
2020-03-30 19:24:25 -0700

    - fixes for network detection - detect errors on trust_ope_certs.cmd - catch errors on apply gpo - retry if failed - detect msvc runtimes already installed

--- bin/restore_gpo.cmd
+++ bin/restore_gpo.cmd
@@ -1,9 +1,37 @@
 @echo off
 
+rem escape code for colors
+SET ESC=[
+SET ESC_CLEAR=%ESC%2j
+SET ESC_RESET=%ESC%0m
+SET ESC_GREEN=%ESC%32m
+SET ESC_RED=%ESC%31m
+SET ESC_YELLOW=%ESC%33m
+
+
+:instgpo
+
 cd %~dp0
 
-echo Restoring local GPO settings...
+echo %ESC_GREEN%Restoring local GPO settings...%ESC_RESET%
 %~dp0lgpo.exe /g "%~dp0rc\gpo" 
+echo %ERRORLEVEL%
+if %ERRORLEVEL% == 0 (
+    echo %ESC_GREEN%success!%ESC_RESET%
+) else (
+    echo.
+    echo. 
+    echo %ESC_RED%===== Error applying gpo settings!!! =====%ESC_RESET%
+    echo.
+    echo.
+    echo %ESC_YELLOW% It is VERY important that this completes to secure the laptop!!! %ESC_RESET%
+    echo.
+    choice /C y /T 3 /D y /M " Do you want to try again? (yes you have to complete this to secure the laptop!!!!)"
+    rem if errorlevel 2 goto endcredential
+    
+    goto instgpo
+)
+
 rem 2>NUL 1<NUL
 
 
