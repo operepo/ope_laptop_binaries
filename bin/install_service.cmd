d885890d9f02f3f05fcd736b8ec761db68ac8eff
Ray Pulsipher <ray@cmagic.biz>
2020-05-25 14:22:38 -0700

    .

--- bin/install_service.cmd
+++ bin/install_service.cmd
@@ -16,18 +16,30 @@ if %ERRORLEVEL% NEQ 0 (
     echo %ESC_YELLOW%STOP OPEService Failed - This isn't an issue if it is the first time you are credentialing this laptop. %ESC_RESET%
 )
 
+echo %ESC_GREEN%UnRegistering OPEService...%ESC_RESET%
+%programdata%\ope\Services\OPEService\OPEService.exe remove
+rem if %ERRORLEVEL% NEQ 0 (
+rem     echo.
+rem     echo %ESC_RED%ERROR UnRegistering OPE Service! - Something wen't VERY wrong. %ESC_RESET%
+rem     echo.
+    rem echo %ESC_YELLOW%Running Bad Credential Fallback%ESC_RESET%
+    rem call %programdata%\ope\Services\mgmt\mgmt.exe bad_credential
+rem     exit /B 2
+rem )
+
 rem need to copy the OPEService folder into the proper location
 
 echo %ESC_GREEN%Copying latest version of Services...%ESC_RESET%
+rem /Q for quiet, /F for full
+SET QUIET_FLAG=/Q
+
 if exist OPEService.py (
-    rem echo In dev folder  %~dp0
-    rem %~dp0\dist\svc\OPEService.exe -install -auto -interactive
     rem /Q instead of F
-    xcopy /ECIHRKY /Q %~dp0\dist\* %programdata%\ope\Services\ 
+    echo -- Copying %~dp0\dist\ to %programdata%\ope\Services\
+    xcopy /ECIHRKY %QUIET_FLAG% %~dp0\dist\* %programdata%\ope\Services\ 
 ) else (
-    rem echo In binary folder
-    rem %~dp0\..\svc\OPEService.exe -install -auto -interactive
-    xcopy /ECIHRKY /Q %~dp0\..\Services\* %programdata%\ope\Services\
+    echo -- Copying %~dp0\dist\ to %programdata%\ope\Services\
+    xcopy /ECIHRKY %QUIET_FLAG% %~dp0\..\Services\* %programdata%\ope\Services\
     
 )
 
@@ -41,7 +53,16 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\%SERVICE_NAME%" 
 rem Install the service - ensure it is installed w the proper settings 
 rem --startup
 echo %ESC_GREEN%Registering OPEService...%ESC_RESET%
-%programdata%\ope\Services\OPEService\OPEService.exe -auto -interactive install 
+rem --interactive
+%programdata%\ope\Services\OPEService\OPEService.exe install
+if %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo %ESC_RED%ERROR Registering OPE Service! - Something wen't VERY wrong. %ESC_RESET%
+    echo.
+    echo %ESC_YELLOW%Running Bad Credential Fallback%ESC_RESET%
+    call %programdata%\ope\Services\mgmt\mgmt.exe bad_credential
+    exit /B 2
+)
 
 echo %ESC_GREEN%Enabling OPEService AutoRecovery Options...%ESC_RESET%
 rem Set service recovery options
@@ -51,11 +72,12 @@ SC failure %SERVICE_NAME% reset=0 actions=restart/60000/restart/60000/reboot/100
 rem sc failure %SERVICE_NAME% command= ""C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "C:\AT\MyPowerShellScript.ps1" "possibleArguments""
 
 echo %ESC_GREEN%Starting OPEService...%ESC_RESET%
-net start OPEService3
+net start OPEService
 if %ERRORLEVEL% NEQ 0 (
     echo.
     echo %ESC_RED%ERROR Starting OPE Service! - Something wen't VERY wrong. %ESC_RESET%
     echo.
+    echo %ESC_YELLOW%Running Bad Credential Fallback%ESC_RESET%
     call %programdata%\ope\Services\mgmt\mgmt.exe bad_credential
     exit /B 2
 )
