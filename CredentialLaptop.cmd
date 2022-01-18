9c2c0fc587f5fb0c796759da8d667de3f351cca6
Raymond Pulsipher <ray@cmagic.biz>
2022-01-17 18:33:26 -0800

    - Check in changes for KMS server from Mike Huse - Bump version number to trigger update

--- CredentialLaptop.cmd
+++ CredentialLaptop.cmd
@@ -68,13 +68,15 @@ echo.
 rem Contributions from Mike Huse - https://github.com/operepo/ope/issues/86
 :dtime_start
 echo %ESC_GREEN%[ ---- Updating System Time ---- ]%ESC_RESET%
-%windir%\system32\date /T
-%windir%\system32\time /T
+date /T
+time /T
 echo Is the system date/time correct?
-choice /C yn /T 15 /D y /M "Press N to set date/time [y/n]"
-if errorlevel 1 goto skipsetdatetime
-%windir%\system32\time
-%windir%\system32\date
+choice /N /C YN /T 10 /D Y /M "Press N to set date/time [y/n]"
+IF ERRORLEVEL ==2 GOTO setdatetime
+IF ERRORLEVEL ==1 GOTO skipsetdatetime
+:setdatetime
+time
+date
 
 :skipsetdatetime
 
@@ -84,31 +86,33 @@ echo %ESC_GREEN%[ ---- Configuring Laptop to get its Windows licensing ---- ]%ES
 
 rem  ***** Change IP Address and KMS name for your server below. You can also replace the IP address with the KMS name as well ******
 echo Do you want to set KMS and Office activation servers?
-choice /C yn /D n /T 10 /M "Press Y to set activation servers [y/n]"
+choice /C yn /T 10 /D n /M "Press Y to set activation servers [y/n]"
 if errorlevel 2 goto skipsetkmsserver
 set KMS_URL=
 rem TODO - Pull this from the registry?
 set DEFAULT_KMS_URL=172.29.20.115
 set OFFICE_URL=
 set DEFAULT_OFFICE_URL=wwcc-kms.wwcc-wsp.edu
-SET OFFICE_PATH=c:\program files (x86)\microsoft office\office16\
-
+SET OFFICE_PATH="c:\program files (x86)\microsoft office\office16\"
+REM **Just trying something**
+echo Default KMS IP is: %DEFAULT_KMS_URL%
+REM cscript %windir%\system32\slmgr.vbs /skms %DEFAULT_KMS_URL%
 SET /p KMS_URL=Enter KMS Server URL/IP [default %DEFAULT_KMS_URL%]:
-if "%KMS_URL%"==""
-    SET KMS_URL=%DEFAULT_KMS_URL%
+if %errorlevel%==1 SET KMS_URL=%DEFAULT_KMS_URL%
     
 echo Setting KMS URL: %KMS_URL%
-%windir%\system32\cscript slmgr.vbs /skms %KMS_URL%
-%windir%\system32\cscript slmgr.vbs /ato
+cscript %windir%\system32\slmgr.vbs /skms %KMS_URL%
+cscript %windir%\system32\slmgr.vbs /ato
 
 
 
 SET /p OFFICE_URL=Enter Office Server URL/IP [default %DEFAULT_OFFICE_URL%]:
-if "%OFFICE_URL%"==""
-    SET KMS_URL=%DEFAULT_OFFICE_URL%
+if %errorlevel%==1 SET OFFICE_URL=%DEFAULT_OFFICE_URL%
 
-%windir%\system32\cscript %OFFICE_PATH%ospp.vbs /sethst:%OFFICE_URL%
-%windir%\system32\cscript %OFFICE_PATH%cscript ospp.vbs /act
+echo Setting Office KMS URL: %OFFICE_URL%
+
+cscript %OFFICE_PATH%ospp.vbs /sethst:%OFFICE_URL%
+cscript %OFFICE_PATH%ospp.vbs /act
 
 :skipsetkmsserver
 
