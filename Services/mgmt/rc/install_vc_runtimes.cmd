172f3709a6f2fb8006ef92da3a56f97efd9bd454
Raymond Pulsipher <ray@cmagic.biz>
2022-03-18 16:26:24 -0700

    - Fix dependency errors for python 3.9.9

--- Services/mgmt/rc/install_vc_runtimes.cmd
+++ Services/mgmt/rc/install_vc_runtimes.cmd
@@ -154,31 +154,27 @@ if %ERRORLEVEL% NEQ 0 (
 
 
 
-rem ---- MSVC 2015-2022 ----
-rem 2015-2019 4891423FE0A523640952AA610E87A0B4
-rem 2015-2022 203A181AD6F3DAB4798A4A626A94D987
-set REG_KEY=HKLM\SOFTWARE\Classes\Installer\Products\203A181AD6F3DAB4798A4A626A94D987
-set MSVC_VERSION=MSVC 2015-2022 x64
+rem ---- MSVC 2015-2019 ----
+set REG_KEY=HKLM\SOFTWARE\Classes\Installer\Products\4891423FE0A523640952AA610E87A0B4
+set MSVC_VERSION=MSVC 2015-2019 x64
 set MSG=%ESC_GREEN%---- Checking install of !MSVC_VERSION!... ----%ESC_RESET%
 echo !MSG!
 reg query "!REG_KEY!" >> nul 2>&1
 if %ERRORLEVEL% NEQ 0 (
  echo %ESC_RED%    - !MSVC_VERSION! not installed, installing now...%ESC_RESET%
- call %~dp0VC_redist.x64_15_to_22.exe /install /quiet /norestart
+ call %~dp0vc_redist.x64_15_to_19.exe /install /quiet /norestart
 ) else (
  echo %ESC_GREEN%    - !MSVC_VERSION! already installed.%ESC_RESET%
 )
 
-rem 2015-2019 1AF6F69BF0351F24F917335C38173604
-rem 2015-2022 3E8F7AED9B7BC3C349B5F7C89E407184
-set REG_KEY=HKLM\SOFTWARE\Classes\Installer\Products\3E8F7AED9B7BC3C349B5F7C89E407184
-set MSVC_VERSION=MSVC 2015-2022 x86
+set REG_KEY=HKLM\SOFTWARE\Classes\Installer\Products\1AF6F69BF0351F24F917335C38173604
+set MSVC_VERSION=MSVC 2015-2019 x86
 set MSG=%ESC_GREEN%---- Checking install of !MSVC_VERSION!... ----%ESC_RESET%
 echo !MSG!
 reg query "!REG_KEY!" >> nul 2>&1
 if %ERRORLEVEL% NEQ 0 (
  echo %ESC_RED%    - !MSVC_VERSION! not installed, installing now...%ESC_RESET%
- call %~dp0VC_redist.x86_15_to_22.exe /install /quiet /norestart
+ call %~dp0vc_redist.x86_15_to_19.exe /install /quiet /norestart
 ) else (
  echo %ESC_GREEN%    - !MSVC_VERSION! already installed.%ESC_RESET%
 )
